e5f09351c67ba385273dfc41a35da933d3e3f57a
Gus Beane <angus.beane@cfa.harvard.edu>
2023-03-20 17:07:57 -0400

    Add load-modules.sh

---
+++ load-modules.sh
@@ -0,0 +1,10 @@
+module purge
+module load gcc/7.1.0-fasrc01
+module load openmpi/3.1.3-fasrc01
+module load gsl
+module load hdf5
+module load python/3.6.3-fasrc02
+module load ffmpeg/4.0.2-fasrc01
+
+export SGRBAR_PATH=`pwd`
+
